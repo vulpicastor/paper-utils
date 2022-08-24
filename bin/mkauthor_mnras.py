8da40e4ec7f4100551ed25617be9a439fb6befaa
Lizhou Sha <lizhou@lsha.net>
2022-08-24 18:13:39 -0500

    mkauthor_mnras.py: Add option to generate email lists

--- bin/mkauthor_mnras.py
+++ bin/mkauthor_mnras.py
@@ -15,6 +15,7 @@ class AuthorList:
     def __init__(self):
         self.author_dict = {}
         self.affil_dict = {}
+        self.email_dict = {}
         self.notes_dict = {}
         self.orcid_dict = {}
         # By convention, affiliations are 1-indexed.
@@ -24,7 +25,7 @@ class AuthorList:
     def _normalize(s):
         return unicodedata.normalize('NFKC', s.strip())
 
-    def add_author_entry(self, name, affiliations, notes=None, orcid_id=None):
+    def add_author_entry(self, name, affiliations, notes=None, email=None, orcid_id=None):
         normalized_name = self._normalize(name)
         if normalized_name in self.author_dict:
             raise ValueError(f'Duplicate author name "{normalized_name}"')
@@ -42,6 +43,10 @@ class AuthorList:
         self.author_dict[normalized_name] = affil_indices
         if notes:
             self.notes_dict[normalized_name] = notes
+        if email:
+            self.email_dict[normalized_name] = email
+        else:
+            logger.warning('No email specified for author "%s"', normalized_name)
         if orcid_id:
             self.orcid_dict[normalized_name] = orcid_id
 
@@ -80,6 +85,12 @@ class AuthorList:
         lines.append(r'\\')
         return lines
 
+    def format_email_list(self):
+        email_list = []
+        for name, email in self.email_dict.items():
+            email_list.append(f'"{name}" <{email}>')
+        return email_list
+
     def format_affiliation_list(self):
         return [self.texify_affiliation_entry(str(i), affil)
                 for affil, i in self.affil_dict.items()]
@@ -89,6 +100,9 @@ class AuthorList:
         affil_lines = self.format_affiliation_list()
         latex_macro = '\n'.join(itertools.chain(author_lines, affil_lines))
         return latex_macro
+
+    def output_email_addr(self):
+        return ', '.join(self.format_email_list())
 
 
 
@@ -118,6 +132,8 @@ def read_csv(f, args):
             author_entry['notes'] = note_list
         if orcid_id := row[columns['orcid']].strip():
             author_entry['orcid_id'] = orcid_id
+        if email := row[columns['email']].strip():
+            author_entry['email'] = email
         author_entries.append(author_entry)
     return author_entries
 
@@ -127,6 +143,9 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='Generate MNRAS-style author list from CSV.',
         prog='mkauthor_mnras.py')
+    parser.add_argument(
+        '--emails', '-e', action='store_true',
+        help='Output names and emails.')
     parser.add_argument(
         '--max-affil', type=int, default=4, metavar='N',
         help='Maximum number of columns for affiliations in the CSV file.')
@@ -149,6 +168,9 @@ def main():
     author_list = AuthorList()
     for entry in author_entries:
         author_list.add_author_entry(**entry)
+    if args.emails:
+        print(author_list.output_email_addr())
+        return
     print(author_list.output_latex(
         with_orcid=args.with_orcid,
         orcid_logo=args.orcid_logo))
